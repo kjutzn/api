c8993cda0cfc34d86da7ddb6b7621027749a77fd
Kjutzn <130908680+kjutzn@users.noreply.github.com>
2023-10-15 21:50:21 +0200

    yes
    no

---
+++ Nesusedni/Nesusedni.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int n1,n2;
+    char c1, c2;
+
+    printf("Unesite Perino i Petrino omiljeno slovo: ");
+    scanf("%c%c", &c1, &c2);
+
+    printf("Unesite broj Perinih i Petrinih omiljenih slova: ");
+    scanf("%d %d", &n1, &n2);
+    
+    if (n1 == n2) {
+        for (int i = 0; i < n1; i++)
+        {
+            printf("%c%c", c1, c2);
+        }
+    }
+
+    else if ((n1 > n2) && (n1 == n2 + 1)) {
+        for (int i = 0; i < n2; i++)
+        {
+            printf("%c%c", c1, c2);
+        }
+        printf("%c", c1);
+    }
+    
+    else if ((n2 > n1) && (n2 == n1 + 1)) {
+        for (int i = 0; i < n1; i++)
+        {
+            printf("%c%c", c2, c1);
+        }
+        printf("%c", c2);
+    }
+    
+    else {
+        printf("Nemoguce\n");
+    }
+    
+    return 0;
+}
