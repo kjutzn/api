73054c5532cca6748e1e15cddaa0624370c99a08
Kjutzn <130908680+kjutzn@users.noreply.github.com>
2023-10-16 20:35:01 +0200

    💀

---
+++ SudarBrojeva/sudarBrojeva.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    long long int x, y;
+    long long int rez = 0;
+    long long int k = 1;
+
+    printf("Unesite prvi broj: ");
+    scanf("%lld", &x);
+
+    printf("Unesite drugi broj: ");
+    scanf("%lld", &y);
+    
+    while (x > 0 || y > 0)
+    {
+        long long int bx = x % 10;
+        long long int by = y % 10;
+        
+        if (bx > by)
+        {
+            rez = rez + (bx * k);
+        }
+        
+        else if (bx < by)
+        {
+            rez = rez + (by * k);
+        }
+        
+        else
+        {
+            rez = rez + (bx * k);
+        }
+        
+        x /= 10;
+        y /= 10;
+        k *= 10;
+    }
+    
+    printf("%lld\n", rez);
+    
+    return 0;
+}
